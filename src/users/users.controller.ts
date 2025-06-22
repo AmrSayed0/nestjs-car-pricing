175cf85f53091f4bc09073e0a6a6c5d853807485
AmrSayed0 <amracademycode@gmail.com>
2025-06-22 16:41:42 +0300

    feat(users): enhance user retrieval with NotFoundException handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from './dtos/user.dtos';
 import { UsersService } from './users.service';
@@ -21,8 +22,10 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   @Get()
